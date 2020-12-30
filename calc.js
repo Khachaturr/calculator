7b5e65ad6aeda8ce6a8f4a6481411a5b4efcebb3
Khachaturr <grantovich91@mail.ru>
2020-12-30 11:50:41 +0400

    first commit

---
+++ calc.js
@@ -0,0 +1,21 @@
+function insert(num){
+    document.form.textview.value=document.form.textview.value + num;
+};
+
+function back(){
+    let exp =document.form.textview.value;
+    document.form.textview.value=exp.substring(0, exp.length-1);
+    }
+    function clean(){
+        document.form.textview.value="";
+    }
+    function sqrt(){
+        
+        document.form.textview.value=Math.sqrt(document.form.textview.value)
+    }
+    
+    function equal(){
+            document.form.textview.value= eval(document.form.textview.value);
+        }
+  
+    
